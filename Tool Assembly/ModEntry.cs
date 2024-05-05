92cd39fd0eaa826ea43810c4e0561ccaec1a138e
OFTS_CQM <qianmuchen.sam@gmail.com>
2024-05-05 10:40:29 -0700

    add menu

--- Tool Assembly/ModEntry.cs
+++ Tool Assembly/ModEntry.cs
@@ -4,13 +4,20 @@ using StardewModdingAPI.Utilities;
 using StardewValley;
 using StardewValley.GameData.Tools;
 using StardewValley.Inventories;
+using StardewValley.Network;
+using Netcode;
+using StardewValley.GameData.BigCraftables;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.VisualBasic;
 
 namespace Tool_Assembly
 {
     public class ModEntry : Mod
     {
         public ModConfig Config = new ModConfig();
-        public Dictionary<int, Inventory> metaData = new();
+        public static readonly NetLongDictionary<Inventory, NetRef<Inventory>> metaData = new();
+        public static readonly NetLongDictionary<int, NetInt> indices = new();
+        public static readonly NetInt topIndex = new();
 
         public override void Entry(IModHelper helper)
         {
@@ -31,9 +38,9 @@ namespace Tool_Assembly
                         Name = "toolAssembly",
                         AttachmentSlots = 0,
                         SalePrice = 1000,
-                        DisplayName = "[LocalizedText Strings\\ofts_toolass:display]",
-                        Description = "[LocalizedText Strings\\ofts_toolass:descrip]",
-                        Texture = "asset/texture",
+                        DisplayName = "[LocalizedText Strings\\ofts_toolass:display_tool]",
+                        Description = "[LocalizedText Strings\\ofts_toolass:descrip_tool]",
+                        Texture = "toolAss/asset/texture",
                         SpriteIndex = 0,
                         MenuSpriteIndex = -1,
                         UpgradeLevel = -1,
@@ -43,25 +50,102 @@ namespace Tool_Assembly
                     datas.Add("ofts.toolAss", data);
                 });
             }
+            else if (args.NameWithoutLocale.IsEquivalentTo("Data/BigCraftables"))
+            {
+                args.Edit(asset =>
+                {
+                    IDictionary<string, BigCraftableData> datas = asset.AsDictionary<string, BigCraftableData>().Data;
+                    BigCraftableData data = new()
+                    {
+                        Name = "ToolConfigTable",
+                        DisplayName = "[LocalizedText Strings\\ofts_toolass:display_table]",
+                        Description = "[LocalizedText Strings\\ofts_toolass:descrip_table]",
+                        Price = 100,
+                        Fragility = 0,
+                        IsLamp = false,
+                        Texture = "toolAss/asset/texture",
+                        SpriteIndex = 21,
+                    };
+                    datas.Add("ofts.toolConfig", data);
+                });
+            }
+            else if (args.NameWithoutLocale.IsEquivalentTo("Data/CraftingRecipies"))
+            {
+                args.Edit(asset =>
+                {
+                    IDictionary<string, string> datas = asset.AsDictionary<string, string>().Data;
+                    datas.Add("Tool Configuation Table", "166 1 338 10 335 5/Home/ofts.toolConfig 1/true/default/");
+                });
+            }
+            else if (args.NameWithoutLocale.IsEquivalentTo("toolAss/asset/texture"))
+            {
+                args.LoadFromModFile<Texture2D>("assets/texture", AssetLoadPriority.Low);
+            }
+            else if (args.NameWithoutLocale.IsEquivalentTo("Strings\\ofts_toolass"))
+            {
+                args.LoadFromModFile<Dictionary<string, string>>("assets/translations", AssetLoadPriority.Low);
+            }
         }
         
         public void ButtonPressed(object? sender, ButtonPressedEventArgs args)
         {
             if (Context.IsWorldReady && Game1.activeClickableMenu == null && 
                 Game1.player.ActiveItem is Tool tool && tool.modData.TryGetValue("ofts.toolAss.id", out var id)
-                && metaData.TryGetValue(int.Parse(id), out var inventory))
+                && long.TryParse(id, out var longid) && metaData.TryGetValue(longid, out var inventory))
             {
                 int indexPlayer = Game1.player.Items.IndexOf(tool);
-                int indexData = inventory.IndexOf(Game1.player.ActiveItem);
 
                 if (Config.Next.JustPressed())
                 {
-                    Game1.player.Items[indexPlayer] = inventory[(indexData + 1) % inventory.Count];
+                    Game1.player.Items[indexPlayer] = inventory[(indices[longid] + 1) % inventory.Count];
                 }
 
                 else if(Config.Prev.JustPressed())
                 {
-                    Game1.player.Items[indexPlayer] = inventory[(indexData + inventory.Count - 1) % inventory.Count];
+                    Game1.player.Items[indexPlayer] = inventory[(indices[longid] + inventory.Count - 1) % inventory.Count];
+                }
+            }
+
+            bool keyPressed = false;
+            foreach(var key in Game1.options.actionButton)
+            {
+                if (args.IsDown(key.ToSButton()))
+                {
+                    keyPressed = true;
+                    break;
+                }
+            }
+
+            if (!keyPressed) return;
+
+            if (Game1.player.currentLocation.Objects.TryGetValue(Game1.player.GetGrabTile(), out var obj)
+                && obj.QualifiedItemId == "(BC)ofts.toolConfig")
+            {
+                clickConfigTable();
+            }
+        }
+
+        public void assignNewInventory(Tool tool)
+        {
+            tool.modData.Add("ofts.toolAss.id", $"{topIndex.Value++}");
+            Inventory inv = new Inventory() { tool };
+            inv.OnSlotChanged += (a, b, c, d) => { a.RemoveEmptySlots(); };
+            metaData.Add(topIndex.Value, inv);
+            indices.Add(topIndex.Value, 0);
+        }
+
+        public void clickConfigTable()
+        {
+            if (Context.IsWorldReady && Game1.activeClickableMenu == null)
+            {
+                if(Game1.player.ActiveItem is Tool tool && (tool.Name == "toolAssembly" || 
+                    tool.modData.ContainsKey("ofts.toolAss.id")))
+                {
+                    if (!tool.modData.ContainsKey("ofts.toolAss.id")){
+                        assignNewInventory(tool);
+                    }
+
+                    Game1.activeClickableMenu = new ToolConfigMenu(long.Parse(tool.modData["ofts.toolAss.id"]));
                 }
             }
         }
---
+++ Tool Assembly/ToolConfigMenu.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace Tool_Assembly
+{
+    public class ToolConfigMenu : IClickableMenu
+    {
+        InventoryMenu ToolInventory;
+        InventoryMenu PlayerInventory;
+        Item? grabed;
+
+        public ToolConfigMenu(long id) : base(0, 0, 0, 0, true){
+            ToolInventory = new(0, 0, false, ModEntry.metaData[id]);
+            PlayerInventory = new(0, 0, false, highlightMethod: (item) => { return item == null || item is Tool; });
+            height = ToolInventory.height + PlayerInventory.height + 50;
+            width = ToolInventory.width;
+            Vector2 pos = Utility.getTopLeftPositionForCenteringOnScreen(height, width);
+            xPositionOnScreen = (int)pos.X;
+            yPositionOnScreen = (int)pos.Y;
+            ToolInventory.xPositionOnScreen = xPositionOnScreen;
+            ToolInventory.yPositionOnScreen = yPositionOnScreen;
+            PlayerInventory.xPositionOnScreen = xPositionOnScreen;
+            PlayerInventory.yPositionOnScreen = yPositionOnScreen + PlayerInventory.height + 50;
+        }
+
+        public override void receiveLeftClick(int x, int y, bool playSound = true)
+        {
+            base.receiveLeftClick(x, y, playSound);
+            grabed = PlayerInventory.leftClick(x, y, grabed);
+            grabed = ToolInventory.leftClick(x, y, grabed);
+        }
+
+        public override void receiveRightClick(int x, int y, bool playSound = true)
+        {
+            base.receiveRightClick(x, y, playSound);
+            grabed = PlayerInventory.rightClick(x, y, grabed);
+            grabed = ToolInventory.rightClick(x, y, grabed);
+        }
+
+        public override void gameWindowSizeChanged(Rectangle oldBounds, Rectangle newBounds)
+        {
+            height = ToolInventory.height + PlayerInventory.height + 50;
+            width = ToolInventory.width;
+            Vector2 pos = Utility.getTopLeftPositionForCenteringOnScreen(height, width);
+            xPositionOnScreen = (int)pos.X;
+            yPositionOnScreen = (int)pos.Y;
+            ToolInventory.xPositionOnScreen = xPositionOnScreen;
+            ToolInventory.yPositionOnScreen = yPositionOnScreen;
+            PlayerInventory.xPositionOnScreen = xPositionOnScreen;
+            PlayerInventory.yPositionOnScreen = yPositionOnScreen + PlayerInventory.height + 50;
+        }
+    }
+}
