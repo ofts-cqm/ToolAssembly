d25515d1e37e21b0e235132e9a5faba79184627a
OFTS_CQM <qianmuchen.sam@gmail.com>
2024-05-07 18:21:16 -0700

    add comments

--- Tool Assembly/IToolAPI.cs
+++ Tool Assembly/IToolAPI.cs
@@ -13,14 +13,33 @@ namespace Tool_Assembly
         /// <returns>the content of this id as an <see cref="Inventory"/></returns>
         Inventory getToolContentWithID(long id);
         /// <summary>
-        /// 
+        /// Get the content inside this tool
         /// </summary>
-        /// <param name="tool"></param>
-        /// <returns></returns>
+        /// <param name="tool">the tool</param>
+        /// <returns>the content of this tool as an <see cref="Inventory"/></returns>
         Inventory getToolContentWithTool(Tool tool);
+        /// <summary>
+        /// Create a new tool
+        /// </summary>
+        /// <param name="assignNewInventory">should assign a new id to this tool or leave blank</param>
+        /// <returns>a new Tool Assembly</returns>
         GenericTool createNewTool(bool assignNewInventory = false);
+        /// <summary>
+        /// Create a new tool with specific id. 
+        /// </summary>
+        /// <param name="id">the id of the tool</param>
+        /// <returns>a new Tool Assembly</returns>
         GenericTool createToolWithId(long id);
+        /// <summary>
+        /// Assign a new inventory. 
+        /// </summary>
+        /// <returns>the assigned id</returns>
         long assignNewInventory();
+        /// <summary>
+        /// return whether the specific id exists. 
+        /// </summary>
+        /// <param name="id">the id</param>
+        /// <returns>exists for not</returns>
         bool doesIDExist(long id);
     }
 }
